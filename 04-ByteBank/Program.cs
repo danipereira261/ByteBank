b17630b16e9ce042f864fb51a30cf07316977d63
dpere <dpere@DESKTOP-B719JFE>
2021-08-13 10:24:07 -0300

    Adicionar arquivos de projeto.

---
+++ 04-ByteBank/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+            
+            contaDoBruno.Sacar(50);
+            contaDoBruno.Depositar(500);
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+           
+            Console.WriteLine("Gabriela, saldo antes de transferir: " + contaDaGabriela.saldo);
+            Console.WriteLine("Bruno, saldo antes de transferir: " + contaDoBruno.saldo);
+
+            contaDaGabriela.Transferir(50, contaDoBruno);
+
+            Console.WriteLine("Bruno, saldo após transferência: " + contaDoBruno.saldo);
+            Console.WriteLine("Gabriela, saldo após transferência: " + contaDaGabriela.saldo);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Metodo();
+            }
+            catch (DivideByZeroException excecao)
+            {
+                Console.WriteLine("Não é possível divisão por 0!");
+            }
+            catch (Exception excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+            Console.ReadLine();
+        }
+        public static int Dividir(int numero, int divisor)
+        {
+            try
+            {
+                return numero / divisor;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
+                throw;
+            }
+        }
+
+        static void Metodo()
+        {
+            try
+            {
+                TestaDivisao(0);
+            }
+            catch (NullReferenceException excecao)
+            {
+                Console.WriteLine(excecao.Message);
+                Console.WriteLine(excecao.StackTrace);
+            }
+        }
+
+        static void TestaDivisao(int divisor)
+        {
+            Dividir(10, divisor);
+        }
+
+    }
+}
